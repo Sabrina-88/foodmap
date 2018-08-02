f9bf6219438ece632710e3155b5bf5baf3e261f5
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-08-02 15:05:28 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-// Add your JavaScript
+$(document).ready(function(){
+$("#btnOrder").click(function(){
+        var foodorder = $("#order").val();;
+        console.log(foodorder);
+    });
+	
+	$.each(restaurantes,function (chave, valor){
+		
+		console.log(valor.name); 
+		
+		if (valor.name === "Sabrina") {
+			 alert("restaurante Sabrina");
+			}
+			
+			else {
+				alert ("só tem restaurante > " + valor.name)}
+		/*console.log($(this).text());*/
+		});
+	/*
+	var data = { "programs": [ { "name":"zonealarm", "price":"500" }, { "name":"kaspersky", "price":"200" } ] };
+
+$.each(data.programs, function (i) {
+    $.each(data.programs[i], function (key, val) {
+        alert(key + " : " + val);
+    });
+});*/
+	
+}); // FINAL DA  FUNCAO READY
+
